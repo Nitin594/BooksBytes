62b71b9ebcb0f70c1ab1e7019538facf769d7ef0
Nitin594 <nitinmaurya594@gmail.com>
2025-10-26 22:43:47 +0530

    ...

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
             to="/"
             className="flex items-center space-x-2 text-white hover:text-gray-200 transition-colors"
           >
-            <span className="text-2xl h-8 w-8"><img src='../././public/bbLogo.png'/></span>
+            <span className="text-2xl h-8 w-8"><img src='../././public/bbLogo.png' alt='logo'/></span>
             <span className="text-xl font-bold mx-0">BooksByte</span>
           </Link>
 
